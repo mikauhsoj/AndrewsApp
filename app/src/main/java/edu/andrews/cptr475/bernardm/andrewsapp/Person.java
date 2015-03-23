1bf18dcac254771dead9950f9f7861152c77d35a
Josh <mikauhsoj@gmail.com>
2015-03-22 23:52:32 -0700

    Started implementation of classes for directory function of app.

---
+++ app/src/main/java/edu/andrews/cptr475/bernardm/andrewsapp/Person.java
@@ -0,0 +1,12 @@
+package edu.andrews.cptr475.bernardm.andrewsapp;
+
+/**
+ * Created by Josh on 3/22/2015.
+ */
+public class Person {
+    private String FirstName;
+    private String LastName;
+    private String MiddleInitial;
+
+
+}
