eb04186c39373697dac87ce587aa0db0b0d4e143
Shemaiah Telemaque <shemaiah@andrews.edu>
2015-03-28 19:15:12 -0400

    Added some more packages.

---
+++ app/src/main/java/edu/andrews/cptr475/bernardm/andrewsapp/RssNewFeed/BlankFragment.java
@@ -0,0 +1,33 @@
+package edu.andrews.cptr475.bernardm.andrewsapp.RssNewFeed;
+
+
+import android.os.Bundle;
+import android.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import edu.andrews.cptr475.bernardm.andrewsapp.R;
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class BlankFragment extends Fragment {
+
+
+    public BlankFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        TextView textView = new TextView(getActivity());
+        textView.setText(R.string.hello_blank_fragment);
+        return textView;
+    }
+
+
+}
